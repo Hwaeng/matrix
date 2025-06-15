8e45b08cef644dca9142c570427b186f238ef351
Hwaeng <hwaengineers@outlook.com>
2025-06-15 18:06:07 +0100

    Add files via upload

---
+++ sites/default/files/js/js_pqzvw6olDMXQLc-jOTPeklGSvNGwMhCscijW5iZmBAs.js
@@ -0,0 +1,540 @@
+/**
+ * @file
+ * better_exposed_filters.js
+ *
+ * Provides some client-side functionality for the Better Exposed Filters module.
+ */
+
+(function ($, Drupal, drupalSettings) {
+  Drupal.behaviors.betterExposedFilters = {
+    attach: function (context, settings) {
+      // Add highlight class to checked checkboxes for better theming.
+      $('.bef-tree input[type=checkbox], .bef-checkboxes input[type=checkbox]')
+        // Highlight newly selected checkboxes.
+        .change(function () {
+          _bef_highlight(this, context);
+        })
+        .filter(':checked').closest('.form-item', context).addClass('highlight');
+    }
+  };
+
+  /*
+   * Helper functions
+   */
+
+  /**
+   * Adds/Removes the highlight class from the form-item div as appropriate.
+   */
+  function _bef_highlight(elem, context) {
+    $elem = $(elem, context);
+    $elem.attr('checked')
+      ? $elem.closest('.form-item', context).addClass('highlight')
+      : $elem.closest('.form-item', context).removeClass('highlight');
+  }
+
+})(jQuery, Drupal, drupalSettings);
+;
+/**
+ * @file
+ * auto_submit.js
+ *
+ * Provides a "form auto-submit" feature for the Better Exposed Filters module.
+ */
+
+(function ($, Drupal, once) {
+
+  /**
+   * To make a form auto submit, all you have to do is 3 things:.
+   *
+   * Use the "better_exposed_filters/auto_submit" js library.
+   *
+   * On gadgets you want to auto-submit when changed, add the
+   * data-bef-auto-submit attribute. With FAPI, add:
+   * @code
+   *  '#attributes' => array('data-bef-auto-submit' => ''),
+   * @endcode
+   *
+   * If you want to have auto-submit for every form element, add the
+   * data-bef-auto-submit-full-form to the form. With FAPI, add:
+   * @code
+   *   '#attributes' => array('data-bef-auto-submit-full-form' => ''),
+   * @endcode
+   *
+   * If you want to exclude a field from the bef-auto-submit-full-form auto
+   * submission, add an attribute of data-bef-auto-submit-exclude to the form
+   * element. With FAPI, add:
+   * @code
+   *   '#attributes' => array('data-bef-auto-submit-exclude' => ''),
+   * @endcode
+   *
+   * Finally, you have to identify which button you want clicked for autosubmit.
+   * The behavior of this button will be honored if it's ajaxy or not:
+   * @code
+   *  '#attributes' => array('data-bef-auto-submit-click' => ''),
+   * @endcode
+   *
+   * Currently only 'select', 'radio', 'checkbox' and 'textfield' types are
+   * supported. We probably could use additional support for HTML5 input types.
+   */
+  Drupal.behaviors.betterExposedFiltersAutoSubmit = {
+    attach: function (context) {
+      // When exposed as a block, the form #attributes are moved from the form
+      // to the block element, thus the second selector.
+      // @see \Drupal\block\BlockViewBuilder::preRender
+      var selectors = 'form[data-bef-auto-submit-full-form], [data-bef-auto-submit-full-form] form, [data-bef-auto-submit]';
+
+      // The change event bubbles so we only need to bind it to the outer form
+      // in case of a full form, or a single element when specified explicitly.
+      $(selectors, context).addBack(selectors).each(function (i, e) {
+        // Store the current form.
+        var $form = $(e);
+
+        // Retrieve the autosubmit delay for this particular form.
+        var autoSubmitDelay = $form.data('bef-auto-submit-delay') || 500;
+
+        // Attach event listeners.
+          $(once('bef-auto-submit', $form))
+          // On change, trigger the submit immediately.
+          .on('change', triggerSubmit)
+          // On keyup, wait for a specified number of milliseconds before
+          // triggering autosubmit. Each new keyup event resets the timer.
+          .on('keyup', Drupal.debounce(triggerSubmit, autoSubmitDelay));
+      });
+
+      /**
+       * Triggers form autosubmit when conditions are right.
+       *
+       * - Checks first that the element that was the target of the triggering
+       *   event is `:text` or `textarea`, but is not `.hasDatePicker`.
+       * - Checks that the keycode of the keyup was not in the list of ignored
+       *   keys (navigation keys etc).
+       *
+       * @param {object} e - The triggering event.
+       */
+      function triggerSubmit(e) {
+        // e.keyCode: key.
+        var ignoredKeyCodes = [
+          16, // Shift.
+          17, // Ctrl.
+          18, // Alt.
+          20, // Caps lock.
+          33, // Page up.
+          34, // Page down.
+          35, // End.
+          36, // Home.
+          37, // Left arrow.
+          38, // Up arrow.
+          39, // Right arrow.
+          40, // Down arrow.
+          9, // Tab.
+          13, // Enter.
+          27  // Esc.
+        ];
+
+        // Triggering element.
+        var $target = $(e.target);
+        var $submit = $target.closest('form').find('[data-bef-auto-submit-click]');
+
+        // Don't submit on changes to excluded elements or a submit element.
+        if ($target.is('[data-bef-auto-submit-exclude], :submit') || ($target.attr('autocomplete') == 'off' && !$target.hasClass('bef-datepicker'))) {
+          return true;
+        }
+
+        // Submit only if this is a non-datepicker textfield and if the
+        // incoming keycode is not one of the excluded values.
+        if (
+          $target.is(':text:not(.hasDatepicker), textarea')
+          && $.inArray(e.keyCode, ignoredKeyCodes) === -1
+        ) {
+          $submit.click();
+        }
+        // Only trigger submit if a change was the trigger (no keyup).
+        else if (e.type === 'change') {
+          $submit.click();
+        }
+      }
+    }
+  }
+
+}(jQuery, Drupal, once));
+;
+/**
+* DO NOT EDIT THIS FILE.
+* See the following change record for more information,
+* https://www.drupal.org/node/2815083
+* @preserve
+**/
+
+(function ($, Drupal) {
+  var states = {
+    postponed: []
+  };
+  Drupal.states = states;
+
+  function invert(a, invertState) {
+    return invertState && typeof a !== 'undefined' ? !a : a;
+  }
+
+  function _compare2(a, b) {
+    if (a === b) {
+      return typeof a === 'undefined' ? a : true;
+    }
+
+    return typeof a === 'undefined' || typeof b === 'undefined';
+  }
+
+  function ternary(a, b) {
+    if (typeof a === 'undefined') {
+      return b;
+    }
+
+    if (typeof b === 'undefined') {
+      return a;
+    }
+
+    return a && b;
+  }
+
+  Drupal.behaviors.states = {
+    attach: function attach(context, settings) {
+      var $states = $(context).find('[data-drupal-states]');
+      var il = $states.length;
+
+      var _loop = function _loop(i) {
+        var config = JSON.parse($states[i].getAttribute('data-drupal-states'));
+        Object.keys(config || {}).forEach(function (state) {
+          new states.Dependent({
+            element: $($states[i]),
+            state: states.State.sanitize(state),
+            constraints: config[state]
+          });
+        });
+      };
+
+      for (var i = 0; i < il; i++) {
+        _loop(i);
+      }
+
+      while (states.postponed.length) {
+        states.postponed.shift()();
+      }
+    }
+  };
+
+  states.Dependent = function (args) {
+    var _this = this;
+
+    $.extend(this, {
+      values: {},
+      oldValue: null
+    }, args);
+    this.dependees = this.getDependees();
+    Object.keys(this.dependees || {}).forEach(function (selector) {
+      _this.initializeDependee(selector, _this.dependees[selector]);
+    });
+  };
+
+  states.Dependent.comparisons = {
+    RegExp: function RegExp(reference, value) {
+      return reference.test(value);
+    },
+    Function: function Function(reference, value) {
+      return reference(value);
+    },
+    Number: function Number(reference, value) {
+      return typeof value === 'string' ? _compare2(reference.toString(), value) : _compare2(reference, value);
+    }
+  };
+  states.Dependent.prototype = {
+    initializeDependee: function initializeDependee(selector, dependeeStates) {
+      var _this2 = this;
+
+      this.values[selector] = {};
+      Object.keys(dependeeStates).forEach(function (i) {
+        var state = dependeeStates[i];
+
+        if ($.inArray(state, dependeeStates) === -1) {
+          return;
+        }
+
+        state = states.State.sanitize(state);
+        _this2.values[selector][state.name] = null;
+        $(selector).on("state:".concat(state), {
+          selector: selector,
+          state: state
+        }, function (e) {
+          _this2.update(e.data.selector, e.data.state, e.value);
+        });
+        new states.Trigger({
+          selector: selector,
+          state: state
+        });
+      });
+    },
+    compare: function compare(reference, selector, state) {
+      var value = this.values[selector][state.name];
+
+      if (reference.constructor.name in states.Dependent.comparisons) {
+        return states.Dependent.comparisons[reference.constructor.name](reference, value);
+      }
+
+      return _compare2(reference, value);
+    },
+    update: function update(selector, state, value) {
+      if (value !== this.values[selector][state.name]) {
+        this.values[selector][state.name] = value;
+        this.reevaluate();
+      }
+    },
+    reevaluate: function reevaluate() {
+      var value = this.verifyConstraints(this.constraints);
+
+      if (value !== this.oldValue) {
+        this.oldValue = value;
+        value = invert(value, this.state.invert);
+        this.element.trigger({
+          type: "state:".concat(this.state),
+          value: value,
+          trigger: true
+        });
+      }
+    },
+    verifyConstraints: function verifyConstraints(constraints, selector) {
+      var result;
+
+      if ($.isArray(constraints)) {
+        var hasXor = $.inArray('xor', constraints) === -1;
+        var len = constraints.length;
+
+        for (var i = 0; i < len; i++) {
+          if (constraints[i] !== 'xor') {
+            var constraint = this.checkConstraints(constraints[i], selector, i);
+
+            if (constraint && (hasXor || result)) {
+              return hasXor;
+            }
+
+            result = result || constraint;
+          }
+        }
+      } else if ($.isPlainObject(constraints)) {
+        for (var n in constraints) {
+          if (constraints.hasOwnProperty(n)) {
+            result = ternary(result, this.checkConstraints(constraints[n], selector, n));
+
+            if (result === false) {
+              return false;
+            }
+          }
+        }
+      }
+
+      return result;
+    },
+    checkConstraints: function checkConstraints(value, selector, state) {
+      if (typeof state !== 'string' || /[0-9]/.test(state[0])) {
+        state = null;
+      } else if (typeof selector === 'undefined') {
+        selector = state;
+        state = null;
+      }
+
+      if (state !== null) {
+        state = states.State.sanitize(state);
+        return invert(this.compare(value, selector, state), state.invert);
+      }
+
+      return this.verifyConstraints(value, selector);
+    },
+    getDependees: function getDependees() {
+      var cache = {};
+      var _compare = this.compare;
+
+      this.compare = function (reference, selector, state) {
+        (cache[selector] || (cache[selector] = [])).push(state.name);
+      };
+
+      this.verifyConstraints(this.constraints);
+      this.compare = _compare;
+      return cache;
+    }
+  };
+
+  states.Trigger = function (args) {
+    $.extend(this, args);
+
+    if (this.state in states.Trigger.states) {
+      this.element = $(this.selector);
+
+      if (!this.element.data("trigger:".concat(this.state))) {
+        this.initialize();
+      }
+    }
+  };
+
+  states.Trigger.prototype = {
+    initialize: function initialize() {
+      var _this3 = this;
+
+      var trigger = states.Trigger.states[this.state];
+
+      if (typeof trigger === 'function') {
+        trigger.call(window, this.element);
+      } else {
+        Object.keys(trigger || {}).forEach(function (event) {
+          _this3.defaultTrigger(event, trigger[event]);
+        });
+      }
+
+      this.element.data("trigger:".concat(this.state), true);
+    },
+    defaultTrigger: function defaultTrigger(event, valueFn) {
+      var oldValue = valueFn.call(this.element);
+      this.element.on(event, $.proxy(function (e) {
+        var value = valueFn.call(this.element, e);
+
+        if (oldValue !== value) {
+          this.element.trigger({
+            type: "state:".concat(this.state),
+            value: value,
+            oldValue: oldValue
+          });
+          oldValue = value;
+        }
+      }, this));
+      states.postponed.push($.proxy(function () {
+        this.element.trigger({
+          type: "state:".concat(this.state),
+          value: oldValue,
+          oldValue: null
+        });
+      }, this));
+    }
+  };
+  states.Trigger.states = {
+    empty: {
+      keyup: function keyup() {
+        return this.val() === '';
+      }
+    },
+    checked: {
+      change: function change() {
+        var checked = false;
+        this.each(function () {
+          checked = $(this).prop('checked');
+          return !checked;
+        });
+        return checked;
+      }
+    },
+    value: {
+      keyup: function keyup() {
+        if (this.length > 1) {
+          return this.filter(':checked').val() || false;
+        }
+
+        return this.val();
+      },
+      change: function change() {
+        if (this.length > 1) {
+          return this.filter(':checked').val() || false;
+        }
+
+        return this.val();
+      }
+    },
+    collapsed: {
+      collapsed: function collapsed(e) {
+        return typeof e !== 'undefined' && 'value' in e ? e.value : !this.is('[open]');
+      }
+    }
+  };
+
+  states.State = function (state) {
+    this.pristine = state;
+    this.name = state;
+    var process = true;
+
+    do {
+      while (this.name.charAt(0) === '!') {
+        this.name = this.name.substring(1);
+        this.invert = !this.invert;
+      }
+
+      if (this.name in states.State.aliases) {
+        this.name = states.State.aliases[this.name];
+      } else {
+        process = false;
+      }
+    } while (process);
+  };
+
+  states.State.sanitize = function (state) {
+    if (state instanceof states.State) {
+      return state;
+    }
+
+    return new states.State(state);
+  };
+
+  states.State.aliases = {
+    enabled: '!disabled',
+    invisible: '!visible',
+    invalid: '!valid',
+    untouched: '!touched',
+    optional: '!required',
+    filled: '!empty',
+    unchecked: '!checked',
+    irrelevant: '!relevant',
+    expanded: '!collapsed',
+    open: '!collapsed',
+    closed: 'collapsed',
+    readwrite: '!readonly'
+  };
+  states.State.prototype = {
+    invert: false,
+    toString: function toString() {
+      return this.name;
+    }
+  };
+  var $document = $(document);
+  $document.on('state:disabled', function (e) {
+    if (e.trigger) {
+      $(e.target).prop('disabled', e.value).closest('.js-form-item, .js-form-submit, .js-form-wrapper').toggleClass('form-disabled', e.value).find('select, input, textarea').prop('disabled', e.value);
+    }
+  });
+  $document.on('state:required', function (e) {
+    if (e.trigger) {
+      if (e.value) {
+        var label = "label".concat(e.target.id ? "[for=".concat(e.target.id, "]") : '');
+        var $label = $(e.target).attr({
+          required: 'required',
+          'aria-required': 'true'
+        }).closest('.js-form-item, .js-form-wrapper').find(label);
+
+        if (!$label.hasClass('js-form-required').length) {
+          $label.addClass('js-form-required form-required');
+        }
+      } else {
+        $(e.target).removeAttr('required aria-required').closest('.js-form-item, .js-form-wrapper').find('label.js-form-required').removeClass('js-form-required form-required');
+      }
+    }
+  });
+  $document.on('state:visible', function (e) {
+    if (e.trigger) {
+      $(e.target).closest('.js-form-item, .js-form-submit, .js-form-wrapper').toggle(e.value);
+    }
+  });
+  $document.on('state:checked', function (e) {
+    if (e.trigger) {
+      $(e.target).prop('checked', e.value);
+    }
+  });
+  $document.on('state:collapsed', function (e) {
+    if (e.trigger) {
+      if ($(e.target).is('[open]') === e.value) {
+        $(e.target).find('> summary').trigger('click');
+      }
+    }
+  });
+})(jQuery, Drupal);;
